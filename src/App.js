566339a4687a53d7ed446f7088412229ecc59e26
Gimna Katugampala <autoservicelk@gmail.com>
2021-08-24 00:26:11 -0700

    create the context socket

--- src/App.js
+++ src/App.js
@@ -5,10 +5,42 @@ import VideoPlayer from './components/VideoPlayer'
 import Options from './components/Options'
 import Notifications from './components/Notifications'
 
+import { makeStyles } from '@material-ui/core/styles'
+
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+      borderRadius: 15,
+      margin: '30px 100px',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '600px',
+      border: '2px solid black',
+  
+      [theme.breakpoints.down('xs')]: {
+        width: '90%',
+      },
+    },
+    image: {
+      marginLeft: '15px',
+    },
+    wrapper: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      width: '100%',
+    },
+  }));
+
 const App = () => {
+
+    const classes = useStyles()
+
     return (
-        <div>
-            <AppBar position="static" color="inherit"> 
+        <div className={classes.wrapper}>
+            <AppBar className={classes.appBar} position="static" color="inherit"> 
                 <Typography variant="h2" align="center">Video Chat</Typography>
             </AppBar>
                 <VideoPlayer/>
