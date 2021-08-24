7eaa97bbd5cc7e9a8fbc60597acaf211a6c493f7
Gimna Katugampala <autoservicelk@gmail.com>
2021-08-23 23:34:17 -0700

    backend and the components setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Typography,AppBar } from '@material-ui/core'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import VideoPlayer from './components/VideoPlayer'
+import Options from './components/Options'
+import Notifications from './components/Notifications'
+
+const App = () => {
+    return (
+        <div>
+            <AppBar position="static" color="inherit"> 
+                <Typography variant="h2" align="center">Video Chat</Typography>
+            </AppBar>
+                <VideoPlayer/>
+
+            <Options>
+                <Notifications />
+            </Options>
+
+        </div>
+    )
 }
 
-export default App;
+export default App
